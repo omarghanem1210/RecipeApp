29bad7ed74fce5557ed00e89b7873afe39e3353b
Mostafa385 <tito72116@gmail.com>
2024-08-10 18:58:32 +0300

    functionality of splash

---
+++ app/src/main/java/com/example/recipeapp/users/view/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.recipeapp.users.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.recipeapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, SignupActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
