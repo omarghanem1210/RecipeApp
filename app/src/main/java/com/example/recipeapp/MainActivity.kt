4670b31b81dda66f65422c885a026b870c179179
samamohamed11 <samamohamed11@gmail.com>
2024-08-10 14:12:51 +0300

    add appbar mainactivity

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.recipeapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+                toolbar = findViewById(R.id.toolbar)
+                setSupportActionBar(toolbar)
+                supportActionBar?.title = "RecipeAPP"
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+                drawerLayout = findViewById(R.id.drawer_layout)
+                val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+                drawerLayout.addDrawerListener(toggle)
+                toggle.syncState()
+
+            }
+
+            override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+                menuInflater.inflate(R.menu.app_bar, menu)
+                return super.onCreateOptionsMenu(menu)
+            }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_about_us -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, about_usFragment())
+                    .addToBackStack(null)
+                    .commit()
+                return true
+            }
+            R.id.action_logout -> {
+                Toast.makeText(this, "action_logout", Toast.LENGTH_LONG).show()
+                return true
+            }
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
+
+
+
 }
+
+
+
